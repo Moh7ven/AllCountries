1082fb287dcbfd282f2fbf6c5806fe8bf01890fb
Moh7ven <Mohamedsangare0107@gmail.com>
2024-01-06 23:36:14 +0000

    Added route configuration in folder Routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        {/* Le path "*" fonctionne si jamais l'url ne correspond à rien de déclaré au dessus */}
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <AppRoutes />;
 };
 
 export default App;
