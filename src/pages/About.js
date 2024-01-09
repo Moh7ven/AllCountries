dd91da7c583b2f05eb8419a23ff49f38499f57ce
Moh7ven <Mohamedsangare0107@gmail.com>
2024-01-09 22:10:43 +0000

    about changed

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,22 +7,52 @@ const About = () => {
     <div>
       <Logo />
       <Navigation />
-      <h1>A propos</h1>
       <br />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus
-        recusandae nostrum reiciendis quos veritatis, cum sunt in? Enim cumque
-        consequuntur doloremque veritatis accusantium iusto praesentium omnis
-        odit velit et itaque, delectus sit! Deserunt temporibus recusandae, odio
-        assumenda sunt, maiores doloremque reprehenderit quidem ab, blanditiis
-        eveniet laboriosam ullam voluptates? Molestias possimus quam vel
-        voluptatum aliquid pariatur alias magnam ea eveniet, officiis beatae eos
-        voluptates quos repellat perspiciatis mollitia maiores iusto aliquam
-        dolorem, sapiente blanditiis. Nulla officiis unde quam rerum ratione
-        assumenda? Iusto illum, culpa voluptates eius laudantium suscipit quae
-        libero deserunt dolor id ducimus minus tempore quis, laborum laboriosam
-        sit corporis?
-      </p>
+      <div className="aboutContainer">
+        <div className="aboutTextContainer">
+          <h1 className="welcomeAbout">
+            Bienvenue sur notre Application "InfosPays" !
+          </h1>
+          <p>
+            Notre application a pour objectif de vous offrir un aperçu détaillé
+            sur chaque pays à travers le monde. Que vous cherchiez des
+            informations sur la superficie, la localisation continentale, ou
+            simplement à découvrir de nouveaux drapeaux, nous sommes là pour
+            vous.
+          </p>
+          <p>Ce que nous offrons :</p>
+
+          <div className="aboutCard">
+            1. Exploration Mondiale : Découvrez et explorez tous les pays du
+            globe, leurs drapeaux, et leurs données essentielles en un seul
+            endroit.
+          </div>
+          <p>
+            2. Informations Complètes : Accédez à une mine d'informations,
+            incluant la localisation continentale, la superficie, la capitale,
+            et bien plus encore.
+          </p>
+          <p>
+            3. Informations Complètes : Accédez à une mine d'informations,
+            incluant la localisation continentale, la superficie, la capitale,
+            et bien plus encore.
+          </p>
+
+          <p>Ce qui nous motive :</p>
+          <p>
+            Nous avons créé cette application avec une motivation simple :
+            combler le fossé de connaissance sur les pays du monde. Souvent, des
+            pays restent méconnus, et notre but est de rendre ces informations
+            accessibles à tous, de manière claire et concise.
+          </p>
+          <p> Notre Engagement : </p>
+          <p>
+            Nous nous engageons à fournir un service de qualité, une expérience
+            utilisateur agréable, et à continuer d'enrichir notre base de
+            données pour vous offrir des informations actualisées.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
